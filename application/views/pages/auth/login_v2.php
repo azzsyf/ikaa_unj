91d821a7d34bb5fce0eebe0f0cfd29225e47def7
azzsyf <m.azizsyafii88@gmail.com>
2024-09-18 10:44:19 +0700

    update2 login v2

--- application/views/pages/auth/login_v2.php
+++ application/views/pages/auth/login_v2.php
@@ -38,8 +38,8 @@
             /* opacity: 0.8; */
         }
 
-        h2{
-            padding-bottom: 50px;
+        h4{
+            padding-bottom: 10px;
         }
 
         .cancelbtn {
@@ -72,16 +72,17 @@
     </style>
 </head>
 <body>
-    <div class="container" style="padding-top: 120px; width: 500px;">
+    <div class="container" style="padding-top: 160px; width: 500px;">
         <?= $this->session->flashdata('message'); ?>
-        <div style="text-align: center; padding: 10px;">
-            <img src="<?= base_url() ?>asset/Logo IKA Akuntansi UNJ bulet.png" width="150px" alt="">
-        </div>
+        
         <div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
         <form id="form-login" method="post" action="" style="padding: 20px; border-radius: 15px; box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);">
             <div class="container">
-                <h2 align="center">Login Form</h2>
-                
+                <h3 align="center" style="font-weight: bold; padding-bottom: 30px;">Selamat Datang</h3>
+                <div style="text-align: center; padding-bottom: 30px;">
+                    <img src="<?= base_url() ?>asset/Logo IKA Akuntansi UNJ bulet.png" width="150px" alt="">
+                </div>
+                <h4 align="center" style="font-weight: bold; padding-bottom: 30px;">Masuk Akun</h4>
                 <button type="submit">Lanjutkan Dengan <img src="<?= base_url() ?>asset/Logo-v2.png" width="150px;" style="padding-left: 20px;" alt=""></button>
             </div>
         </form>
